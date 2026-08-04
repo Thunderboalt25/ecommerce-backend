a71c8f28a2093b327ec30c75b8ac27b8e614be07
Bharat Mehta <mehtabharatkumar97@gmail.com>
2026-08-04 15:16:22 +0530

    Fix image URL

--- products/serializers.py
+++ products/serializers.py
@@ -7,14 +7,25 @@ from categories.serializers import CategorySerializer
 class ProductSerializer(serializers.ModelSerializer):
 
     category = CategorySerializer(read_only=True)
+    image = serializers.SerializerMethodField()
 
     class Meta:
-
         model = Product
-
         fields = "__all__"
+
+    def get_image(self, obj):
+        request = self.context.get("request")
+
+        if obj.image:
+            if request:
+                return request.build_absolute_uri(obj.image.url)
+            return obj.image.url
+
+        return None
+
+
 class ProductCreateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = "__all__"        
+        fields = "__all__"
